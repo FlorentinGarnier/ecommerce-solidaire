f49db2dfb812563665af1af84501a932ead3aced
Florentin Garnier <florentin@digital404.fr>
2020-04-20 18:57:40 +0200

    Ajout de click and collect + maj Sylius 1.7.2

--- sylius/src/Entity/Shipping/Shipment.php
+++ sylius/src/Entity/Shipping/Shipment.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity\Shipping;
 
+use CoopTilleuls\SyliusClickNCollectPlugin\Entity\ClickNCollectShipment;
+use CoopTilleuls\SyliusClickNCollectPlugin\Entity\ClickNCollectShipmentInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Shipment as BaseShipment;
 
@@ -11,6 +13,7 @@ use Sylius\Component\Core\Model\Shipment as BaseShipment;
  * @ORM\Entity
  * @ORM\Table(name="sylius_shipment")
  */
-class Shipment extends BaseShipment
+class Shipment extends BaseShipment implements ClickNCollectShipmentInterface
 {
+    use ClickNCollectShipment;
 }
